8d9910ec0be612ee61fcc0dc44b474a0112be3e6
TwoBitC0der <twobitcoders@gmail.com>
2024-09-17 09:22:32 -0700

    Updated Windows Bat to match existing tests
    Updated the windows bat file to match the updated test cli to make tests uniform on supported platforms.

--- tests/test_cli.bat
+++ tests/test_cli.bat
@@ -1,6 +1,8 @@
+@echo off
 REM
-REM Test command-line interface 
+REM Test command-line interface
 REM
+
 REM Nothing, should display usage
 REM Usage: jx [options] <user script> file...
 REM   -c, --compact-output      Minimize output
@@ -13,58 +15,70 @@ REM   -s, --slurp               Read entire stream into array, run script once
 REM   -S, --sort-keys           Sort object keys
 REM       --tab                 Use tabs instead of spaces
 REM   -V, --version             Print program version and exit
-out\jx 
 
-REM Send a json string on stdin to jx for pretty-printing
-echo '{"foo": 0}' | out\jx 'x'
+echo "Test 0000 ******"
+out\jx
 
-REM Get the json from a file for pretty printing
-out\jx 'x' < data\dragons.json
+REM Send a JSON string on stdin to jx for pretty-printing
+echo "Test 0001 ******"
+echo {"foo": 0} | out\jx "x"
 
-REM Use cat to get the data from a file
-cat data\dragons.json | out\jx 'x'
+REM Get the JSON from a file for pretty printing
+echo "Test 0002 ******"
+type tests\data\dragons.json | out\jx "x"
+
+REM Use type (instead of cat) to get the data from a file
+echo "Test 0003 ******"
+type tests\data\dragons.json | out\jx "x"
 
 REM Accept the name of a file as an argument to get the data
-out\jx 'x' data\dragons.json
+echo "Test 0004 ******"
+out\jx "x" tests\data\dragons.json
 
 REM 2 input files
-out\jx 'x' data\dragons.json data\cats.json
+echo "Test 0005 ******"
+out\jx "x" tests\data\dragons.json tests\data\cats.json
 
 REM --compact-output
 REM Should look ~ like:
 REM [["0","1","42"],{"baz":"1","foo":0,"bar":"42"}]
-cat data\compact_me.txt | out\jx --compact-output 'x' 
+echo "Test 0006 ******"
+type tests\data\compact_me.txt | out\jx --compact-output "x"
 
-REM No idea how to print exit status on Windows
-REM --exit-status 
-REM null 1
-REM false 1
-REM true 0
-echo '"foo"' | out\jx --exit-status 'null' && echo %ERRORLEVEL% 
-echo '"foo"' | out\jx --exit-status '{return false}' && echo %ERRORLEVEL%
-echo '"foo"' | out\jx --exit-status '{return true}' && echo %ERRORLEVEL%
+REM --exit-status (there's no direct equivalent for 'echo $?')
+REM For Windows, %ERRORLEVEL% stores the exit status, so use conditional checks.
+echo "Test 0007 ******"
+echo "foo" | out\jx --exit-status "null"
+echo Exit status: %ERRORLEVEL%
+
+echo "foo" | out\jx --exit-status "{return false}"
+echo Exit status: %ERRORLEVEL%
+
+echo "foo" | out\jx --exit-status "{return true}"
+echo Exit status: %ERRORLEVEL%
 
 REM --from-file
 REM 42
-echo '{"z":1, "foo":2, "x51":3, "1":4, "a":32}' | out\jx --from-file tests\add.js 
+echo "Test 0008 ******"
+echo {"z":1, "foo":2, "x51":3, "1":4, "a":32} | out\jx --from-file tests\add.js
 
-REM --help (see top of file)
-
-REM --indent 
+REM --indent
 REM {
 REM        "foo": 0
 REM }
-echo '{"foo":0}' | out\jx --indent 7
+echo "Test 0009 ******"
+echo {"foo": 0} | out\jx --indent 7
 
 REM --monochrome-output
-REM NA observe no color in output
+REM Observe no color in output
 
 REM --slurp
 REM [
 REM   "foo",
 REM   "bar"
 REM ]
-echo '"foo" "bar"' | out\jx --slurp
+echo "Test 0010 ******"
+echo "foo" "bar" | out\jx --slurp
 
 REM --sort-keys
 REM {
@@ -72,12 +86,17 @@ REM    "bar": 2,
 REM    "baz": 0,
 REM    "foo": 40
 REM }
-echo '{"foo":40,"baz":0,"bar":2}' | out\jx -sort-keys 'x' 
+echo "Test 0011 ******"
+echo {"foo": 40, "baz": 0, "bar": 2} | out\jx --sort-keys "x"
 
-REM --tab 
-echo '{"foo":0}' | out\jx --tab 
+REM --tab
+echo "Test 0012 ******"
+echo {"foo": 0} | out\jx --tab
 
 REM --version
 REM jx n.n.n
+echo "Test 0013 ******"
 out\jx --version
 
+echo "Test 0014 ******"
+type tests\data\dragons.json | out\jx "x.xyz()"
